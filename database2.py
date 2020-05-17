cdf7250ba3a6ab2d3811bcd999744b6f9e71c4d1
marvin evins <maevins@cps.edu>
2020-05-16 23:03:23 -0500

    working on small app with db

---
+++ database2.py
@@ -0,0 +1,17 @@
+import sqlite3
+#connect to database
+conn = sqlite3.connect('customer.db')
+#create a cursor
+c = conn.cursor()
+
+#query the database
+c.execute("SELECT rowid, *FROM customers")
+items = c.fetchall()
+
+
+for item in items:
+    print(item)
+
+
+conn.commit()
+conn.close()
