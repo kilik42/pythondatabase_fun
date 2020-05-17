0fa729e3c813e303642d1af0ec212bf2b38665cb
marvin evins <maevins@cps.edu>
2020-05-16 23:11:38 -0500

    working on new app, made new show all function

--- database2.py
+++ database2.py
@@ -1,17 +1,20 @@
 import sqlite3
-#connect to database
-conn = sqlite3.connect('customer.db')
-#create a cursor
-c = conn.cursor()
 
-#query the database
-c.execute("SELECT rowid, *FROM customers")
-items = c.fetchall()
+#query the DB and return all records
+def show_all():
+    #connect to database
+    conn = sqlite3.connect('customer.db')
+    #create a cursor
+    c = conn.cursor()
+    #query the database
+    c.execute("SELECT rowid, *FROM customers")
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    conn.commit()
+    conn.close()
 
 
-for item in items:
-    print(item)
-
-
-conn.commit()
-conn.close()
+show_all()
